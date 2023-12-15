9b91e17c0197541d21fb830130a6788dd67acb7c
zhangchi <c.zhang25@trip.com>
2023-12-16 00:10:50 +0800

    修改数据库配置信息，现在所有服务都可以启动

--- store-search/src/main/java/com/yecheng/search/config/SearchConfiguration.java
+++ store-search/src/main/java/com/yecheng/search/config/SearchConfiguration.java
@@ -1,43 +1,22 @@
 package com.yecheng.search.config;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- /**
- * @author Yelf
- * @create 2022-11-13-15:29
- *
- * description: 消息队列配置
- */
 @Configuration
 public class SearchConfiguration {
 
     /**
      * mq序列化方式，选择json！
+     *
      * @return
      */
     @Bean
-    public MessageConverter messageConverter(){
+    public MessageConverter messageConverter() {
 
         return new Jackson2JsonMessageConverter();
     }
 
-    /**
-     * es客户端添加到ioc容器
-     * @return
-     */
-    @Bean
-    public RestHighLevelClient restHighLevelClient(){
-        RestHighLevelClient client =
-                new RestHighLevelClient(
-                        RestClient.builder(HttpHost.create("http://localhost:9200")));
-
-        return client;
-    }
 }
